cd529cb84a5e5f9cf4282cdf71aa42754e89b275
Janne Sauvala <janne.sauvala@gmail.com>
2021-06-30 11:14:52 +0300

    Fix table alignment in org-files

--- init.el
+++ init.el
@@ -422,12 +422,12 @@ folder, otherwise delete a word"
 
 ;; Turn on indentation and auto-fill mode for Org files
 (defun js/org-mode-setup ()
-            (org-indent-mode)
-            (variable-pitch-mode 1)
-            (auto-fill-mode 0)
-            (visual-line-mode 1)
-            (setq evil-auto-indent nil)
-            (diminish org-indent-mode))
+             (org-indent-mode)
+             ;(variable-pitch-mode 1) ;; Causes table columns not be aligned
+             (auto-fill-mode 0)
+             (visual-line-mode 1)
+             (setq evil-auto-indent nil)
+             (diminish org-indent-mode))
 
 (use-package org
   :defer t
@@ -491,13 +491,13 @@ folder, otherwise delete a word"
 ;; Increase the size of various headings
 (set-face-attribute 'org-document-title nil :font "JetBrains Mono" :weight 'bold :height 1.3)
 (dolist (face '((org-level-1 . 1.2)
-                (org-level-2 . 1.1)
-                (org-level-3 . 1.05)
-                (org-level-4 . 1.0)
-                (org-level-5 . 1.1)
-                (org-level-6 . 1.1)
-                (org-level-7 . 1.1)
-                (org-level-8 . 1.1)))
+		(org-level-2 . 1.1)
+		(org-level-3 . 1.05)
+		(org-level-4 . 1.0)
+		(org-level-5 . 1.1)
+		(org-level-6 . 1.1)
+		(org-level-7 . 1.1)
+		(org-level-8 . 1.1)))
   (set-face-attribute (car face) nil :font "JetBrains Mono" :weight 'medium :height (cdr face)))
 
 ;; Make sure org-indent face is available
@@ -505,7 +505,7 @@ folder, otherwise delete a word"
 
 ;; Ensure that anything that should be fixed-pitch in Org files appears that way
 (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-;(set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
 (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
 (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
 (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
