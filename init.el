f17072add4b9fab716e426233ca50a05802e45e2
Janne Sauvala <janne.sauvala@gmail.com>
2021-07-01 20:21:26 +0300

    Add Javascript language tooling

--- init.el
+++ init.el
@@ -409,6 +409,44 @@ folder, otherwise delete a word"
                           (require 'lsp-pyright)
                           (lsp))))  ; or lsp-deferred
 
+(use-package nvm
+  :defer t)
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :config
+  (setq typescript-indent-level 2))
+
+(defun js/set-js-indentation ()
+  (setq js-indent-level 2)
+  (setq evil-shift-width js-indent-level)
+  (setq-default tab-width 2))
+
+(use-package js2-mode
+  :mode "\\.jsx?\\'"
+  :config
+  ;; Use js2-mode for Node scripts
+  (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
+
+  ;; Don't use built-in syntax checking
+  (setq js2-mode-show-strict-warnings nil)
+
+  ;; Set up proper indentation in JavaScript and JSON files
+  (add-hook 'js2-mode-hook #'js/set-js-indentation)
+  (add-hook 'json-mode-hook #'js/set-js-indentation))
+
+
+(use-package apheleia
+  :config
+  (apheleia-global-mode +1))
+
+(use-package prettier-js
+  :hook ((js2-mode . prettier-js-mode)
+          (typescript-mode . prettier-js-mode))
+  :config
+  ;(setq prettier-js-show-errors nil)
+  )
+
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
