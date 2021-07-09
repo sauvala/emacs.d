64ec7b28bcefb711356486dfc9dc33ffdeaad5c6
Janne Sauvala <janne.sauvala@gmail.com>
2021-07-09 18:53:17 +0300

    Customise dashboard items list

--- init.el
+++ init.el
@@ -146,6 +146,11 @@
         dashboard-set-heading-icons t
         dashboard-set-file-icons t
         dashboard-center-content t
+        dashboard-items '((recents  . 5)
+                          (bookmarks . 5)
+                          (projects . 5)
+                          (agenda . 5)
+                          (registers . 5))
         dashboard-init-info js/startup-time-message)
   (dashboard-setup-startup-hook)
   :hook ((server-after-make-frame . dashboard-refresh-buffer)
