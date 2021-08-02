502ad7e8aa3a41912f3defb95b62f453c540d996
Janne Sauvala <janne.sauvala@gmail.com>
2021-08-02 10:41:15 +0300

    Add default encoding

--- init.el
+++ init.el
@@ -219,6 +219,19 @@
     "qR"  'restart-emacs
     "qr"  '(js/reload-init :which-key "reload confs")))
 
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+;; backwards compatibility as default-buffer-file-coding-system
+;; is deprecated in 23.2.
+(if (boundp 'buffer-file-coding-system)
+    (setq-default buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8))
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
 (use-package orderless
   :defer 0.1
   :init
