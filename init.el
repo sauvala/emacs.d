6f2e145063a93a8c8066fd6cfabc49dd4f26dd9b
Janne Sauvala <janne.sauvala@gmail.com>
2021-08-11 21:59:36 +0300

    Add tools for centering content and showing a minimap

--- init.el
+++ init.el
@@ -346,6 +346,13 @@ folder, otherwise delete a word"
 (setq-default tab-width 2)
 (setq-default evil-shift-width tab-width)
 
+(use-package centered-window)
+
+(use-package sublimity
+  :config
+  (require 'sublimity-map)
+  (require 'sublimity-attractive))
+
 (use-package magit
   :bind ("C-M-;" . magit-status)
   :commands (magit-status magit-get-current-branch)
